a095f707af7cea1e7d7a7ea5f95e43b92a79bfe3
Ainur <ainurforex@mail.ru>
2022-06-06 21:15:40 +0500

    Урок 2 ДЗ 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte bananWeightGr=80;
+        byte milkWeightGr=105;
+        byte plombirWeightGr=100;
+        byte eggWeightGr=70;
+        float breakfastTotalWeightGr;
+        float breakfastTotalWeightKg;
+        breakfastTotalWeightGr=(bananWeightGr*5)+(milkWeightGr*2)+(plombirWeightGr*2)+(eggWeightGr*4);
+        breakfastTotalWeightKg=breakfastTotalWeightGr/1000;
+        System.out.println("Общий вес завтрака- "+breakfastTotalWeightKg+" кг.");
     }
 }
